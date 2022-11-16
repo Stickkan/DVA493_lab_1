f9d273a0cad827a71c69c881018c84aa00df3e66
Stickkan <fnl18001@student.mdu.se>
2022-11-16 14:04:58 +0100

    Added ANN.py

---
+++ ANN.py
@@ -0,0 +1,44 @@
+import numpy as nm
+import matplotlib.pyplot as plt
+from os import path
+from pandas import read_csv as load_file
+
+
+#Artificial neural network
+
+#Loading the file, since we use pandas no need to open() or close()
+#file = 'Diabetic.txt'
+#file_name = path.join(path.dirname(path.realpath(__file__)), file)
+#data = load_file(file_name)
+
+
+open('c:\Users\fredr\OneDrive\Dokument\Civilingenjör robotik\Pågående kurser\DVA493\Lab_1\Diabetic.txt', 'r')
+
+def training_set():
+    #The training set is supposed to be 75% of entire set
+    final_input_index = int(1150 * .75)
+    return final_input_index
+
+def validation_index(start_index):
+    size_of_set = int(1150 * 0.1)
+    last_index = start_index + size_of_set
+    return last_index
+
+def test_set():
+    first_index = int(1150 * + .15)
+    return first_index
+
+print(f'The training set is from index 0 to {training_set()}\n')
+print(f'The validation set is from index {training_set() + 1} to index {validation_index(training_set() + 1)}')
+print(f'Lastly the test set is from {1150-(test_set() + 1)} to index 1150')
+
+
+#What needs to e done:
+#1) Learn Git
+#2) How to load the dataset
+#3) Divide the dataset into subsets (training/validation/test)
+#4) Create nodes and layers (Ning: No more than 2 hidden layers with 10 nodes in each)
+#5) Forwardpropagation
+#6) Error/cost function
+#7) B(l)ackpropagation
+#8) When do we stop / when is enough enough.
