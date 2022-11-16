10ef58abb19343d0a61137c769efab361ec858a2
Stickkan <fnl18001@student.mdu.se>
2022-11-16 15:01:33 +0100

    yay

--- ANN.py
+++ ANN.py
@@ -7,12 +7,12 @@ from pandas import read_csv as load_file
 #Artificial neural network
 
 #Loading the file, since we use pandas no need to open() or close()
-#file = 'Diabetic.txt'
-#file_name = path.join(path.dirname(path.realpath(__file__)), file)
-#data = load_file(file_name)
+##file = 'Diabetic.txt'
+##file_name = path.join(path.dirname(path.realpath(__file__)), file)
+##data = load_file(file_name)
 
-
-open('c:\Users\fredr\OneDrive\Dokument\Civilingenjör robotik\Pågående kurser\DVA493\Lab_1\Diabetic.txt', 'r')
+#This is not the correct search way
+#open('c:\Users\fredr\OneDrive\Dokument\Civilingenjör robotik\Pågående kurser\DVA493\Lab_1\Diabetic.txt', 'r')
 
 def training_set():
     #The training set is supposed to be 75% of entire set
@@ -33,7 +33,7 @@ print(f'The validation set is from index {training_set() + 1} to index {validati
 print(f'Lastly the test set is from {1150-(test_set() + 1)} to index 1150')
 
 
-#What needs to e done:
+#What needs to be done:
 #1) Learn Git
 #2) How to load the dataset
 #3) Divide the dataset into subsets (training/validation/test)
@@ -43,4 +43,4 @@ print(f'Lastly the test set is from {1150-(test_set() + 1)} to index 1150')
 #7) B(l)ackpropagation
 #8) When do we stop / when is enough enough.
 
-#I will just add a comment to see if there is any difference 
+#I will just add a comment to see if there is any difference...  there was a difference :-)
